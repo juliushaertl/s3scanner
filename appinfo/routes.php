303d997e50ca9368e3797f223ae754ab0c41c14c
Julius Härtl <jus@bitgrid.net>
2021-08-03 10:38:38 +0200

    Use correct controller method for scanning all files through OCS API
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,7 +1,7 @@
 <?php
 return [
 	'ocs' => [
-		['name' => 'Scan#scan', 'url' => '/scan/{userId}', 'verb' => 'POST'],
+		['name' => 'Scan#scanAll', 'url' => '/scan/{userId}', 'verb' => 'POST'],
 		['name' => 'Scan#scan', 'url' => '/scan/{userId}/{path}', 'verb' => 'POST', 'requirements' => array('path' => '.+')],
 	]
 ];
